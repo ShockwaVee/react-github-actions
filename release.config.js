12e8534acbb8a1243a9c9be5b2149fe5e222e364
Davor Tvoric <davor.tvoric@gmail.com>
2021-04-11 17:15:31 +0200

    fix(global): test the semantic release
    A new package has been added, called 'Semantic Release' for semantic
    versioning

---
+++ release.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    branches: "main",
+    repositoryUrl: "https://github.com/ShockwaVee/react-github-actions",
+    plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', '@semantic-release/github']
+}
