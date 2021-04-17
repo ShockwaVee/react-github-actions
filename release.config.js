1ff98fbc9242b4940812287dd93a2503f950e20b
Davor Tvoric <davor.tvoric@gmail.com>
2021-04-17 14:45:46 +0200

    style(global): Revert options

--- release.config.js
+++ release.config.js
@@ -3,12 +3,7 @@ module.exports = {
   repositoryUrl: 'https://github.com/ShockwaVee/react-github-actions',
   plugins: [
     '@semantic-release/commit-analyzer',
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        includeDetails: true,
-      },
-    ],
+    '@semantic-release/release-notes-generator',
     '@semantic-release/github',
   ],
 };
