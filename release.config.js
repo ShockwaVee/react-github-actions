e1fcdaffbfb2cec592801d5b4bc8292fb1874aed
Davor Tvoric <davor.tvoric@gmail.com>
2021-04-17 14:33:28 +0200

    fix(global): Add an option to release config
    Add the include details option

--- release.config.js
+++ release.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  branches: 'main',
-  repositoryUrl: 'https://github.com/ShockwaVee/react-github-actions',
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/github',
-  ],
+    branches: 'main',
+    repositoryUrl: 'https://github.com/ShockwaVee/react-github-actions',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        ['@semantic-release/release-notes-generator', {
+            includeDetails: true
+        }],
+        '@semantic-release/github',
+    ],
 };
