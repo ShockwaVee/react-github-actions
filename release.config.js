817f3e7533741e2278f53a4f30332b05294d3448
Davor Tvoric <davor.tvoric@gmail.com>
2021-04-17 14:34:43 +0200

    style(global): Run linter

--- release.config.js
+++ release.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-    branches: 'main',
-    repositoryUrl: 'https://github.com/ShockwaVee/react-github-actions',
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        ['@semantic-release/release-notes-generator', {
-            includeDetails: true
-        }],
-        '@semantic-release/github',
+  branches: 'main',
+  repositoryUrl: 'https://github.com/ShockwaVee/react-github-actions',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        includeDetails: true,
+      },
     ],
+    '@semantic-release/github',
+  ],
 };
