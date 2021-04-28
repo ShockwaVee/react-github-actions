63c25be3761a567b29962f03e706e354cbe5abae
Davor Tvoric <davor.tvoric@gmail.com>
2021-04-28 12:00:34 +0200

    ci(App): test js actions

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -652,13 +652,22 @@ module.exports = /******/ (function (modules, runtime) {
       const github = __webpack_require__(469);
 
       try {
+        core.debug('debug message');
+        core.warning('warning message');
+        core.error('error message');
+
         const name = core.getInput('who-to-greet');
+        core.setSecret(name);
         console.log(`Hello ${name}`);
 
         const now = new Date();
         core.setOutput('time', now.toTimeString());
 
+        core.startGroup('Logging github object');
         console.log(JSON.stringify(github, null, '\t'));
+        core.endGroup();
+
+        core.exportVariable('HELLO', 'bok davor');
       } catch (error) {
         core.setFailed(error.message);
       }
