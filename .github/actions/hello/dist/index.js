b57e23e8d77f4dbbe96df034cb7a366f20409812
Davor Tvoric <davor.tvoric@gmail.com>
2021-04-28 11:48:33 +0200

    ci(App): test js actions

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -652,6 +652,7 @@ module.exports = /******/ (function (modules, runtime) {
       const github = __webpack_require__(469);
 
       try {
+        throw new Error('an error message');
         const name = core.getInput('who-to-greet');
         console.log(`Hello ${name}`);
 
