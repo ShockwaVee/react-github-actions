f130e39e38d6a81f314aece31b2f69e18318fff5
Davor Tvoric <davor.tvoric@gmail.com>
2023-06-12 09:27:24 +0200

    Update test.js

--- bin/test.js
+++ bin/test.js
@@ -1,1 +1,12 @@
-console.log('test');
+const commits = execSync(`git log master..HEAD --pretty='format:%s%n%n%b${delimiter}'`)
+    .toString()
+    .replaceAll('\r\n', '\n')
+    .split(delimiter)
+    .map(parseMessage)
+    .filter(Boolean)
+    .reduce((acc, commit) => {
+      acc[commit.type] = (acc[commit.type] ?? []).concat(commit);
+      return acc;
+    }, {});
+
+console.log(commits + ' heheo');
